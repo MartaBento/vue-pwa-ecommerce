23e3446a138a0d66be61483b88c830c7d7651e39
Marta Bento <marta.moreira1@gmail.com>
2022-01-03 23:33:26 +0000

    working on the first features

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,5 @@
+const path = require('path')
+
 export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: false,
@@ -46,5 +48,13 @@ export default {
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {},
+  build: {
+    extend(config, ctx) {
+      config.module.rules.push({
+        test: /\.md$/,
+        loader: 'frontmatter-markdown-loader',
+        include: path.resolve(__dirname, 'contents'),
+      })
+    }
+  },
 }
